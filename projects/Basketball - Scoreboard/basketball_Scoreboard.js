dd12cfd020ac00e354081c1584dba3e4a8c330c6
Selma Karagøz <selma.krgz@outlook.com>
2024-02-16 18:39:39 +0100

    fixed a bug that caused wrong score results

--- projects/Basketball - Scoreboard/basketball_Scoreboard.js
+++ projects/Basketball - Scoreboard/basketball_Scoreboard.js
@@ -1,36 +1,37 @@
-let homeNUM = document.getElementById("homeNum")
-let guestNUM = document.getElementById("guestNum")
-let result = 0;
+let homeNUM = document.getElementById("homeNum");
+let guestNUM = document.getElementById("guestNum");
+let homeResult = 0;
+let guestResult = 0;
 
-homeNUM.textContent = result;
-guestNUM.textContent = result;
+homeNUM.textContent = homeResult;
+guestNUM.textContent = guestResult;
 
-function homeAdd1(){
-    result++;
-    homeNUM.textContent = result;
+function homeAdd1() {
+    homeResult++;
+    homeNUM.textContent = homeResult;
 }
 
-function homeAdd2(){
-    result+=2;
-    homeNUM.textContent = result;
+function homeAdd2() {
+    homeResult += 2;
+    homeNUM.textContent = homeResult;
 }
 
-function homeAdd3(){
-    result+=3;
-    homeNUM.textContent = result;
+function homeAdd3() {
+    homeResult += 3;
+    homeNUM.textContent = homeResult;
 }
 
-function guestAdd1(){
-    result++;
-    guestNUM.textContent = result;
+function guestAdd1() {
+    guestResult++;
+    guestNUM.textContent = guestResult;
 }
 
-function guestAdd2(){
-    result+=2;
-    guestNUM.textContent = result;
+function guestAdd2() {
+    guestResult += 2;
+    guestNUM.textContent = guestResult;
 }
 
-function guestAdd3(){
-    result+=3;
-    guestNUM.textContent = result;
+function guestAdd3() {
+    guestResult += 3;
+    guestNUM.textContent = guestResult;
 }
